ed404ff12c3923334dea0b522bebd58021ec9a84
Tri Nguyen <nvutri@gmail.com>
2013-11-02 01:16:19 -0500

    Move FightData to FighterData to reflect better the class purpose

---
+++ UFCFighterApp/src/yftvn/ufc/FighterData.java
@@ -0,0 +1,53 @@
+package yftvn.ufc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.Log;
+
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+/**
+ * Class to query Fighters Data. Assuming that Parse.initialize before calling
+ * any methods here.
+ * 
+ * @author nvutri
+ */
+public class FighterData {
+
+	// Fighter info table.
+	private static final String FIGHTER_TABLE = "Fighters";
+	private static final String FIGHTER_TABLE_ESPNID = "espnId";
+
+	/**
+	 * Query a Fighter from the database given his espnId.
+	 * 
+	 * @param espnId
+	 * @throws ParseException
+	 * @return
+	 */
+	public static Fighter getFighter(int espnId) {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery(FIGHTER_TABLE);
+		query.whereEqualTo(FIGHTER_TABLE_ESPNID, espnId);
+		ParseObject fighterParse = new ParseObject(FIGHTER_TABLE);
+		List<ParseObject> fighterList;
+		try {
+			fighterList = query.find();
+			fighterParse = fighterList.get(0);
+			assert 1 == fighterList.size();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new Fighter(espnId, fighterParse);
+	}
+
+}
