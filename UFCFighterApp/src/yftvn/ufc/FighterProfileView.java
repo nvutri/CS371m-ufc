daa1a3653044d063e3db3a51181938287470d394
Tri Nguyen <nvutri@gmail.com>
2013-11-03 19:46:43 -0600

    Cleanup FighterProfileView + Implement the getPhotoURL. Next step will be moving activity code to here.

--- UFCFighterApp/src/yftvn/ufc/FighterProfileView.java
+++ UFCFighterApp/src/yftvn/ufc/FighterProfileView.java
@@ -1,16 +1,21 @@
 package yftvn.ufc;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
+/**
+ * Class to display Fighter Information.
+ */
 public class FighterProfileView extends View {
-
-	private Bitmap fighterPic;
+	/**
+	 * The URL Format to get the photo of the fighter based on his espnId. The
+	 * format request 3 integers: espnId, width, height.
+	 */
+	private static final String PHOTO_URL_FORMAT = "http://a.espncdn.com/combiner/i?img=/i/headshots/mma/players/full/%d.png&w=%d&h=%d";
+	private static final int PHOTO_DEFAULT_HEIGHT = 150;
+	private static final int PHOTO_DEFAULT_WIDTH = 150;
 
 	public FighterProfileView(Context context) {
 		super(context);
@@ -24,20 +29,20 @@ public class FighterProfileView extends View {
 		super(context, attrs);
 	}
 
-	public void initialize(String name) {
-		fighterPic = BitmapFactory.decodeResource(getResources(),
-				R.drawable.anderson_silva);
-		// I want to call R.drawable.name, but it won't let me pass in the name
-		// as a variable
+	public void initialize(int espnId) {
 	}
 
 	@Override
 	public void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-
-		canvas.drawBitmap(fighterPic, null, // src
-				new Rect(0, 0, 250, 250), // dest
-				null);
 	}
 
+	/**
+	 * @param espnId
+	 * @return String of the correct Photo URL to be displayed.
+	 */
+	private String getPhotoURL(int espnId) {
+		return String.format(PHOTO_URL_FORMAT, espnId, PHOTO_DEFAULT_HEIGHT,
+				PHOTO_DEFAULT_WIDTH);
+	}
 }
