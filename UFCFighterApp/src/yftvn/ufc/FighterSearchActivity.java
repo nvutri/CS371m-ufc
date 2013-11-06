ef2795c2f75470c6cf2ce54c6e00126e74262218
RainFang <YCFang87@gmail.com>
2013-11-06 14:48:54 -0600

    text filtering
    text filtering implemented. however, the onclick isn't working properly
    if you use text filtering because it's basing the click index on the old
    array

--- UFCFighterApp/src/yftvn/ufc/FighterSearchActivity.java
+++ UFCFighterApp/src/yftvn/ufc/FighterSearchActivity.java
@@ -8,9 +8,12 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.parse.Parse;
@@ -29,7 +32,10 @@ public class FighterSearchActivity extends Activity {
 
 	private static final String PARSE_APPLICATION_ID = "AJ0JAEbsMNs3pRi9poiROGLxopvwD9Y44aXs8rkz";
 	private static final String PARSE_CLIENT_KEY = "ia1k06D9lHgWncELjHm49xsrbREVWUCn7flMc0ic";
-
+	
+	// User input to be searched with
+	EditText inputSearch;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -61,8 +67,10 @@ public class FighterSearchActivity extends Activity {
 	 */
 	private void displayListView() {
 		ListView lv = (ListView) findViewById(R.id.listView);
-		listAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, fighterNames);
+		
+		inputSearch = (EditText) findViewById(R.id.inputSearch);
+		
+		listAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.fighter_name, fighterNames);
 		lv.setAdapter(listAdapter);
 		// React to user clicks on item
 		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -75,6 +83,31 @@ public class FighterSearchActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+		
+        /**
+         * Enabling Search Filter
+         * */
+        inputSearch.addTextChangedListener(new TextWatcher() {
+             
+            @Override
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                // When user changed the Text
+                FighterSearchActivity.this.listAdapter.getFilter().filter(cs);   
+            }
+             
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                    int arg3) {
+                // TODO Auto-generated method stub
+                 
+            }
+             
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub                          
+            }
+        });
+        
 		// we register for the contextmneu
 		registerForContextMenu(lv);
 	}
