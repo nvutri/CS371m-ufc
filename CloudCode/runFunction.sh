52797b21f883aca3264bd8357058b8713f68f342
Tri Nguyen <nvutri@gmail.com>
2013-11-08 10:10:17 -0600

    Initial Commit for Cloud Code.

---
+++ CloudCode/runFunction.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+  -H "X-Parse-Application-Id: AJ0JAEbsMNs3pRi9poiROGLxopvwD9Y44aXs8rkz" \
+  -H "X-Parse-REST-API-Key: ASvFj4V0kHOndsSBM2iao1oxKzqCxtaLw2w2plid" \
+  -H "Content-Type: application/json" \
+  -d '{}' \
+  https://api.parse.com/1/functions/$1
