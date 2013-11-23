5b76921f39013751c4d72592d78445d85d31a177
Tri Nguyen <nvutri@gmail.com>
2013-11-23 14:16:33 -0600

    Linting over FightAdapter

--- UFCFighterApp/src/yftvn/ufc/adapters/FightAdapter.java
+++ UFCFighterApp/src/yftvn/ufc/adapters/FightAdapter.java
@@ -13,51 +13,49 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class FightAdapter extends ArrayAdapter<FightEvent>
-{
-	Context context; 
-    int layoutResourceId;    
-    ArrayList<FightEvent> data = null;
-    
-    public FightAdapter(Context context, int layoutResourceId, ArrayList<FightEvent> data) {
-        super(context, layoutResourceId, data);
-        this.layoutResourceId = layoutResourceId;
-        this.context = context;
-        this.data = data;
-    }
+/**
+ * FightAdapter to display FightEvent List with fighter names.
+ */
+public class FightAdapter extends ArrayAdapter<FightEvent> {
+	private Context context;
+	private int layoutResourceId;
+	private ArrayList<FightEvent> fightEvents;
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-        FightHolder holder = null;
-        
-        if(row == null)
-        {
-            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-            row = inflater.inflate(layoutResourceId, parent, false);
-            
-            holder = new FightHolder();
-            holder.left = (TextView)row.findViewById(R.id.left);
-            holder.right = (TextView)row.findViewById(R.id.right);
-            
-            row.setTag(holder);
-        }
-        else
-        {
-            holder = (FightHolder)row.getTag();
-        }
-        
-        FightEvent fightEvent = data.get(position);
-        holder.left.setText(fightEvent.getLeft());
-        holder.right.setText(fightEvent.getRight());
-        
-        return row;
-    }
-    
-    static class FightHolder
-    {
-        TextView left;
-        TextView right;
-    }
+	public FightAdapter(Context context, int layoutResourceId,
+			ArrayList<FightEvent> fightEvents) {
+		super(context, layoutResourceId, fightEvents);
+		this.layoutResourceId = layoutResourceId;
+		this.context = context;
+		this.fightEvents = fightEvents;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		FightHolder holder = null;
+
+		if (row == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			row = inflater.inflate(layoutResourceId, parent, false);
+
+			holder = new FightHolder();
+			holder.left = (TextView) row.findViewById(R.id.left);
+			holder.right = (TextView) row.findViewById(R.id.right);
+
+			row.setTag(holder);
+		} else {
+			holder = (FightHolder) row.getTag();
+		}
+		// Populate ListView with fighter names.
+		FightEvent fightEvent = fightEvents.get(position);
+		holder.left.setText(fightEvent.getFirstFighter());
+		holder.right.setText(fightEvent.getSecondFighter());
+		return row;
+	}
+
+	static class FightHolder {
+		TextView left;
+		TextView right;
+	}
 
 }
