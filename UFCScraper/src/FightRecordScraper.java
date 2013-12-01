b9d5b77722819cf74e49579aca3a8672bc89a8ca
Tri Nguyen <nvutri@gmail.com>
2013-11-30 20:13:51 -0600

    Some cleanup on the FightRecord code.

--- UFCScraper/src/FightRecordScraper.java
+++ UFCScraper/src/FightRecordScraper.java
@@ -2,10 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
