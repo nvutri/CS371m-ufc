0ee3cd76e07af9555dc660e3c05260ac8555860a
Tri Nguyen <nvutri@gmail.com>
2013-12-01 11:37:56 -0600

    Bash code push script of Anderson Silva and another fighter.

---
+++ UFCCloudCode/matchesHistory.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+  -H "X-Parse-Application-Id: AJ0JAEbsMNs3pRi9poiROGLxopvwD9Y44aXs8rkz" \
+  -H "X-Parse-REST-API-Key: ASvFj4V0kHOndsSBM2iao1oxKzqCxtaLw2w2plid" \
+  -H "Content-Type: application/json" \
+  -d '{"fighter":2335447, "opponent": 2335525}' \
+  https://api.parse.com/1/functions/matchesHistory
