0cfd7653bcf5360db5158cc5ef714cbefc8cc590
Tri Nguyen <nvutri@gmail.com>
2013-12-06 00:26:50 -0600

    Lint fightEvent

--- UFCFighterApp/src/yftvn/ufc/activities/FightEventActivity.java
+++ UFCFighterApp/src/yftvn/ufc/activities/FightEventActivity.java
@@ -26,16 +26,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class FightEventActivity extends Activity {
-	
+
 	// A tag for the cat log
 	private static final String TAG = "Event";
-	
+
 	/**
 	 * The event ID and name for the UFC event this activity will display.
 	 */
 	private int eventId;
 	private String eventName;
-	
+
 	private ArrayList<FightEvent> fightEvents;
 
 	/**
@@ -44,28 +44,28 @@ public class FightEventActivity extends Activity {
 	private static TextView mEventTextView;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) 
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.event);
-		
+
 		Bundle bundle = getIntent().getExtras();
 		eventId = bundle.getInt("eventId");
 		eventName = bundle.getString("eventName");
-		
-		Log.d(TAG, "Event ID is: "+eventId);
-		Log.d(TAG, "Event Name is: "+eventName);
-		
+
+		Log.d(TAG, "Event ID is: " + eventId);
+		Log.d(TAG, "Event Name is: " + eventName);
+
 		// Get all fight Events from Parse.
 		fightEvents = FightEventData.getAllFightEvents(eventId);
-		
+
 		initHeaderView();
 		// Displaying all the fight events.
 		FightAdapter adapter = new FightAdapter(this, R.layout.event_row,
 				fightEvents);
 		ListView listViewE = (ListView) findViewById(R.id.listViewE);
-		//View header = (View) getLayoutInflater().inflate(R.layout.event_header, null);
-		//listViewE.addHeaderView(header);
+		// View header = (View)
+		// getLayoutInflater().inflate(R.layout.event_header, null);
+		// listViewE.addHeaderView(header);
 		listViewE.setAdapter(adapter);
 
 		listViewE.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -80,52 +80,46 @@ public class FightEventActivity extends Activity {
 			}
 		});
 	}
-	
+
 	/**
 	 * Associate the header TextView field with an object.
 	 */
-	private void initHeaderView() 
-	{
+	private void initHeaderView() {
 		mEventTextView = (TextView) findViewById(R.id.eventHeader);
 		mEventTextView.setText(eventName);
 	}
-	
-	@Override 
-	public boolean onCreateOptionsMenu(Menu menu) 
-	{ 
-		super.onCreateOptionsMenu(menu); 
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
 
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.options_menu, menu);
 		return true;
 
 	}
-	
+
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) 
-	{
-		switch (item.getItemId()) 
-		{
-			case R.id.event_menu:
-				eventMenu();
-				return true;
-				
-			case R.id.fighter_search_menu: 
-				fighterSearchMenu();   	
-				return true;
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.event_menu:
+			eventMenu();
+			return true;
+
+		case R.id.fighter_search_menu:
+			fighterSearchMenu();
+			return true;
 		}
 		return false;
 	}
-	
-	public void eventMenu()
-	{
+
+	public void eventMenu() {
 		Intent intent = new Intent(FightEventActivity.this,
 				FightEventListActivity.class);
 		startActivity(intent);
 	}
-	
-	public void fighterSearchMenu()
-	{
+
+	public void fighterSearchMenu() {
 		Intent intent = new Intent(FightEventActivity.this,
 				FighterSearchActivity.class);
 		startActivity(intent);
